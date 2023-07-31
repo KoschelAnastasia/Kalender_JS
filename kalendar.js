687e2863ee34d922f06ade244693b06268eb338b
Anastasia Koschel <anastasia_koschel@teilnehmer.cdemy.de>
2023-07-31 14:23:30 +0200

    version 2.1 mit kommentare

--- kalendar.js
+++ kalendar.js
@@ -10,13 +10,13 @@ function überschrift()
     let dateD = date.getDate();
     let monthD = date.getMonth ();
     let year = date.getFullYear ();
-    //Hab ein wert für Constante gegeben, damit Monats wÜrde als Zahlen gezeigt 
+    //Zueignung ein wert für Konstanten, damit Monats wÜrde als Zahlen gezeigt 
     const monthName = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
     let monthInfo = (monthName[monthD]);
     
     
     
-    //Hab ID für Variablen gegeben, damit ich die in HTML aufrufen könnte
+    //Zueignung einer ID zu Variablen, damit ich die in HTML aufrufen könnte
     document.getElementById(`date`).innerHTML = dateD;
     document.getElementById(`month`).innerHTML = monthInfo;
     document.getElementById(`year`).innerHTML = year;
@@ -30,17 +30,17 @@ function kalendarInfotext()
     let monthD = date.getMonth ();
     let year = date.getFullYear ();
     let weekdayD = date.getDay ();
-    //Hab ein wert für Constante gegeben, damit Monats wÜrde als Text gezeigt
+    //Zueignung ein wert für Konstanten, damit Monats wurde als Text gezeigt.
     const monthName = ["Januar", "Februar", "März", "April ", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
     let monthInfo = (monthName[monthD]);
-    //Hab ein wert für Constante gegeben, damit Wochentags wÜrde als Text gezeigt. Sonntag hat in getDay Wert von 0, es heißt Sonntag "erste Tag" in der Woche ist.
+    //Zueignung ein wert für Konstanten, damit Wochentags wurde als Text gezeigt. Sonntag hat in getDay Wert von 0, es heißt Sonntag "erste Tag" in der Woche ist.
     const weekDayName = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
     let weekDayInfo = (weekDayName[weekdayD]);
 
     // let weekInMonth = date.getMonth ();
     // let holidayYesNoD;
     
-    //Hab ID für Variablen gegeben, damit ich die in HTML aufrufen könnte
+    //Zueignung einer ID zu Variablen, damit die in HTML aufgerufen werden können
     document.getElementById(`infoDate`).innerHTML = dateD;
     document.getElementById(`infoMonth`).innerHTML = monthInfo;
     document.getElementById(`infoMonth2`).innerHTML = monthInfo;
