071dbe2a25dfb98e7711c6b20e1622ab52f8a364
Anastasia Koschel <anastasia_koschel@teilnehmer.cdemy.de>
2023-07-31 16:56:11 +0200

    version 2.1.1 mit  feiertage funktion

--- kalendar.js
+++ kalendar.js
@@ -1,46 +1,44 @@
+//Funktion Liste
 window.onload= function() {
     überschrift();
     kalendarInfotext();
+    holidays();
+    wIMonth ();
 }
-
+//Funktion für oberes Überschrift
 function überschrift()
 {
-    //Variablen für überschrift
+//Variablen für überschrift
     let date= new Date ();
     let dateD = date.getDate();
     let monthD = date.getMonth ();
     let year = date.getFullYear ();
-    //Zueignung ein wert für Konstanten, damit Monats wÜrde als Zahlen gezeigt 
+//Zueignung ein wert für Konstanten, damit Monats würde als Zahlen gezeigt 
     const monthName = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
     let monthInfo = (monthName[monthD]);
-    
-    
-    
-    //Zueignung einer ID zu Variablen, damit ich die in HTML aufrufen könnte
+//Zueignung einer ID zu Variablen, damit die in HTML aufgerufen werden können
     document.getElementById(`date`).innerHTML = dateD;
     document.getElementById(`month`).innerHTML = monthInfo;
     document.getElementById(`year`).innerHTML = year;
 }
-
+//Funktion für Information Kapitel
 function kalendarInfotext()
 {    
-    //Variablen für infoText
+//Variablen für infoText
     let date= new Date ();
     let dateD = date.getDate();
     let monthD = date.getMonth ();
     let year = date.getFullYear ();
     let weekdayD = date.getDay ();
-    //Zueignung ein wert für Konstanten, damit Monats wurde als Text gezeigt.
+//Zueignung ein wert für Konstanten, damit Monats wurde als Text gezeigt.
     const monthName = ["Januar", "Februar", "März", "April ", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
     let monthInfo = (monthName[monthD]);
-    //Zueignung ein wert für Konstanten, damit Wochentags wurde als Text gezeigt. Sonntag hat in getDay Wert von 0, es heißt Sonntag "erste Tag" in der Woche ist.
+//Zueignung ein wert für Konstanten, damit Wochentags wurde als Text gezeigt. Sonntag hat in getDay Wert von 0, es heißt Sonntag "erste Tag" in der Woche ist.
     const weekDayName = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
     let weekDayInfo = (weekDayName[weekdayD]);
-
     // let weekInMonth = date.getMonth ();
-    // let holidayYesNoD;
     
-    //Zueignung einer ID zu Variablen, damit die in HTML aufgerufen werden können
+//Zueignung einer ID zu Variablen, damit die in HTML aufgerufen werden können
     document.getElementById(`infoDate`).innerHTML = dateD;
     document.getElementById(`infoMonth`).innerHTML = monthInfo;
     document.getElementById(`infoMonth2`).innerHTML = monthInfo;
@@ -49,5 +47,30 @@ function kalendarInfotext()
     document.getElementById(`infoYear`).innerHTML = year;
     document.getElementById(`infoYear2`).innerHTML = year;
 // document.getElementById(`infoHowManyWeekInMonth`).innerHTML = weekInMonth;
-// document.getElementById(`infoHoliday`).innerHTML = holidayYesNoD;
+}
+// Ein Algorithmus der Prüft, ob Heute ein Feiertag ist
+function holidays () {
+    let date= new Date ();
+    let dateD = date.getDate();
+//In getMonth ist der Wert für Moaten ab 0 anfängt. Es heißt, dass Januar=0 und Dezember=11. Um Monate bequemer in Algorithmus eingeben zu können, würde getMonth + 1 geschrieben
+    let monthD = date.getMonth () + 1;
+    let holidayYesNoD;
+//Alle Feiertage in Hessen für Jahr 2023
+    if (dateD == 1 && monthD == 1 || dateD == 7 && monthD == 4 || dateD == 10 && monthD == 4 || dateD == 1 && monthD == 5 || 
+        dateD == 18 && monthD ==5 || dateD == 29 && monthD == 5 || dateD == 8 && monthD ==6 || dateD == 3 && monthD == 10 ||
+        dateD == 25 && monthD == 12 || dateD == 26 && monthD ==12 )
+    then (holidayYesNoD = "")
+    else (holidayYesNoD = "nicht") 
+//Zueignung einer ID zu Variablen, damit die in HTML aufgerufen werden können
+    document.getElementById(`infoHoliday`).innerHTML = holidayYesNoD;
+}
+function wIMonth () {
+    let date = new Date();
+    let weekdayD = date.getDay ();
+    const weekDayName = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+    let weekDayInfo = (weekDayName[weekdayD]);
+    
+
+console.log (weekDayInfo);
+
 }
