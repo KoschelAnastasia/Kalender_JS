7f6613b9f67b8405b3e480ac907304b8b9f4cb07
Anastasia Koschel <anastasia_koschel@teilnehmer.cdemy.de>
2023-08-29 16:03:48 +0200

    2.1.6 mit Osternrechner

--- kalendar.js
+++ kalendar.js
@@ -2,11 +2,8 @@
 window.onload= function() {
     überschrift();
     kalendarInfotext();
-    holidays();
     wIMonth ();
     calenderSheet ();
-    
-
 }
 
 //Funktion für oberes Überschrift
@@ -61,29 +58,10 @@ function kalendarInfotext()
 
 }
 
-// Ein Algorithmus der Prüft, ob Heute ein Feiertag ist
-function holidays () {
-    let date= new Date ();
-    let dateD = date.getDate();
-
-    //In getMonth ist der Wert für Moaten ab 0 anfängt. Es heißt, dass Januar=0 und Dezember=11. Um Monate bequemer in Algorithmus eingeben zu können, würde getMonth + 1 geschrieben
-    let monthD = date.getMonth () + 1;
-    let holidayYesNoD;
-
-    //Alle Feiertage in Hessen für Jahr 2023
-    if (dateD == 1 && monthD == 1 || dateD == 7 && monthD == 4 || dateD == 10 && monthD == 4 || dateD == 1 && monthD == 5 || 
-        dateD == 18 && monthD ==5 || dateD == 29 && monthD == 5 || dateD == 8 && monthD ==6 || dateD == 3 && monthD == 10 ||
-        dateD == 25 && monthD == 12 || dateD == 26 && monthD ==12 )
-    then (holidayYesNoD = "")
-    else (holidayYesNoD = "nicht") 
-
-    //Zueignung einer ID zu Variablen, damit die in HTML aufgerufen werden können
-    document.getElementById(`infoHoliday`).innerHTML = holidayYesNoD;
-}
 
 //Ein Algorithmus zum Zählen von Wiederholungen von Wochentagen
 function wIMonth () {
-
+    
     //Konstanten, die deffinieren wert von Heutigen Tag und Wochentag
     const date = new Date();
     const dateD = date.getDate();
@@ -94,16 +72,16 @@ function wIMonth () {
     while (firstWeekdayOfMonth.getDate() !== weekdayD){
         firstWeekdayOfMonth.setDate(firstWeekdayOfMonth.getDate()+1)
     }
-
+    
     //Matematische Formel funkzionirt auf folgende Weise:
     //dateD hat wert von heutigen Tag. weekdayD hat wert von Wochentag (wo: 0 ist Sonntag, 1 ist Montag usw)
     //firstWeekdayOfMonth hat wert von erste Tag in Monat der gleiche Wochen Tag hat wie weekdayD
     //Matimatische Formel berechnet Differenz zwischen dateD und firstWeekdayOfMonth und addiert weekdayD.
     //Dann wird Ergebnis durch 7 geteilt. Um "Runde" Zahl in Ergebnis zu bekommen, wird hier .ceil Methode verwendet 
     //Mithilfe Methode .ceil wird die Ergebnis immer nach oben "gerundet"   
-        const numberOfWeekday = Math.ceil ((dateD -firstWeekdayOfMonth.getDate() + weekdayD)/7);
-        document.getElementById(`infoNummberWeekday`).innerHTML = numberOfWeekday;
-        
+    const numberOfWeekday = Math.ceil ((dateD -firstWeekdayOfMonth.getDate() + weekdayD)/7);
+    document.getElementById(`infoNummberWeekday`).innerHTML = numberOfWeekday;
+    
 }
 
 
@@ -123,7 +101,7 @@ function calenderSheet () {
     let monthElement = document.getElementById(`monat`);
     let yearElement = document.getElementById(`jahr`);
     
-
+    
     // Aufruf den Buttons in die Kalender Kopf.
     let buttonLinksMonat = document.getElementById(`buttonLinksMonat`);
     let buttonRechtsMonat = document.getElementById(`buttonRechtsMonat`);
@@ -144,11 +122,11 @@ function calenderSheet () {
         //Erste und letzte Tag des Monats in Kalendar Kopf werden richtig geschrieben   
         const firstDayofMonth = new Date (year, monthD, 1);
         const lastDayOfMonth = new Date (year, monthD +1, 0);
-    
-    
+        
+        
         //Variables, die gesamte Zahl von Tagen für bestimte Monat hat    
         let allDayInMonth = lastDayOfMonth.getDate();
-    
+        
         //Variables, die Wochentag von erste Tag des Monats hat    
         let firstDayOfWeek = firstDayofMonth.getDay();
         
@@ -156,15 +134,15 @@ function calenderSheet () {
         if (firstDayOfWeek == 0){
             firstDayOfWeek = 7;
         }
-
+        
         //Variables, die Wochentag von letzte Tag des Monats hat
         lastDayOfWeek = lastDayOfMonth.getDay();
-
+        
         //Schleife "if" die für Sonntag wert 7 macht, damit die Woche von Montag anfangen könnte
         if (lastDayOfWeek == 0){
             lastDayOfWeek = 7;
         }
-
+        
         //Zueignung ein wert für Konstanten, damit Monats wurde als Text gezeigt.
         const monthName = ["Jan.", "Feb.", "März", "Apr. ", "Mai", "Jun.", "Jul.", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."];
         let textMonth = (monthName[monthD]);
@@ -172,30 +150,30 @@ function calenderSheet () {
         //Monat und Jahr Variablen bekommen ein wert. Für Monat ist es ein Text. Für Jahr Zahl
         monthElement.textContent = (textMonth);
         yearElement.textContent = (date.getFullYear());
-
+        
         //Operation, die löscht Inhalt, befor ein neues zu schreiben
         calenderBody.innerHTML = "";
-
+        
         //Konstanten die Datum für lezte Monat definieren
         const prevMonth = new Date(year, monthD - 1, 1);
         const prevMonthLastDay = new Date(year, monthD, 0);
         const daysInPrevMonth = prevMonthLastDay.getDate();
-
+        
         //Konstanten die Datum für nächste Monat definieren
         const nextMonth = new Date(year, monthD + 1, 1);
-
+        
         
         //Eine Schleife die Zahlen des Vormonats definiert. Funktioniert volgendes: Die schleife "sagt", dass i gleich
         //  firsteDayOfWeek minus 1 ist. firstDayWeekder hat immer Wert von Erste Tag den gewünschte Monats, wenn wir substrahiren
         // eins, dann werden wir den letzte Tag vorherieges Monats bekommen. Und bis i großer oder genau so Hoh wie
         // 1, wird immer wieder von i 1 substrahieren
         for (let i = firstDayOfWeek - 1; i >= 1; i--) {
-                
-                const prevMonthDay = new Date(prevMonth);
-                prevMonthDay.setDate(daysInPrevMonth - i + 1);
-                const emptyCell = tagCellCreator(prevMonthDay, `otherMonth`);
-                calenderBody.appendChild(emptyCell);
-
+            
+            const prevMonthDay = new Date(prevMonth);
+            prevMonthDay.setDate(daysInPrevMonth - i + 1);
+            const emptyCell = tagCellCreator(prevMonthDay, `otherMonth`);
+            calenderBody.appendChild(emptyCell);
+            
         }            
         
         
@@ -204,23 +182,23 @@ function calenderSheet () {
         // Variables wert von 30) Und so lange bis erste Tag des Monats hat weniger oder genau so wert wie allDayInMonth
         // wird zum tag 1 summiert
         for (let tag = 1; tag <= allDayInMonth; tag ++) {
-
+            
             let day = new Date(date.getFullYear(), date.getMonth(), tag);
-
+            
             const tagCell = tagCellCreator(day);
-
+            
             calenderBody.appendChild(tagCell);
-
+            
             
             if (dateD == tag && monthD == date2.getMonth() && year == date2.getFullYear()){
-                     
-                        
+                
+                
                 tagCell.classList.add(`today`);
-                             
+                
             }
         }
         
-       
+        
         //Eine Schleife die Zahlen des nächstes Monats definiert
         for (let i = 1; i <= 7 - lastDayOfWeek; i++) {
             
@@ -230,18 +208,98 @@ function calenderSheet () {
             calenderBody.appendChild(emptyCell);
             
         }
-
-    //     holidays()
-
-    //     function holidays() { 
-
-    // }
         
-    }
+        //     holidays()
+        
+        //     function holidays() { 
+            
+            // }
+            
+        }
+        calcOsternDatum()
+        
+        // Ostern Formel 
+        function calcOsternDatum() { 
+            
+            let date= new Date ();
+            let dateD = date.getDate();
+        let monthD = date.getMonth();
+        let year = date.getFullYear();
+        let globalDate = new Date (year, monthD, dateD);
+        console.log("What is globalDate" + globalDate );
+        console.log(`Year is: `  + year);
+    // Diese Variablen werden verwendet, um des Datum des Frühlingvollmonds zu berechnen. Ostern ist ein Feiertag dessen Datum nicht fest schteht.
+    // Die ist immer abhängig von Frühlingvollmonds. Alle Konstanten werden weiter in Gaußschen Formel verwendet, um die Länge der Mondmonate, Sonnentage
+    // und andere Faktoren zu korregieren, um ein genaues Ostern Datum zu erhalten 
+    const a = year % 19; 
+    const b = Math.floor(year / 100);
+    const c = year % 100;
+    const d = Math.floor(b / 4);
+    const e = b % 4;
+    const f = Math.floor((b + 8) / 25);
+    const g = Math.floor((b - f + 1) / 3);
+    const h = (19 * a + b - d - g + 15) % 30;
+    const i = Math.floor(c / 4);
+    const k = c % 4;
+    const l = (32 + 2 * e + 2 * i - h - k) % 7;
+    const m = Math.floor((a + 11 * h + 22 * l) / 451);
+    
+    // Berechnen in welches Monat ist Ostern
+    const month3 = Math.floor((h + l - 7 * m + 114) / 31) -1;
+    
+    // Ostern feiert man immer am Sonntag. Diese Formel berechnet es
+    const date3 = ((h + l - 7 * m + 114) % 31) + 1;
+    
+    let osternDatum = new Date(year, month3 , date3);
+    
+    console.log(`Der Datum von Osternsonntag: ` + osternDatum);
+    
+    
+    
+    calcHessenFerien ()
+    
+    function calcHessenFerien () {
+      
+     
+     
+    
+    let feiertagen = [
+        { date: new Date (year, 0, 1), name: "Neujahrstag" },
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate() -2), name: "Karfreitag"},
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate()), name: "Ostersonntag" },
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate() + 1), name: "Ostermontag"},
+        { date: new Date (year, 4, 1), name: "Tag der Arbeit"},
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate() + 39), name: "Christi Himmelfahrt"},
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate() + 49), name: "Pfingstsonntag"} ,
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate() + 50), name: "Pfingstmontag"},
+        { date: new Date (year, osternDatum.getMonth(), osternDatum.getDate() + 60), name: "Fronleichnam"} ,
+        { date: new Date (year, 9, 3), name: "Tag der Deutschen Einheit"},
+        { date: new Date (year, 11, 25), name: "1. Weihnachtsfeiertag"},
+        { date: new Date (year, 11, 26), name: "2. Weihnachtsfeiertag"},  
+        {date: new Date (year, 7, 29), name: "Test Datum"}
+    
+        ];  
+        console.log(`Und was ist: ` + feiertagen[1].date);
+        console.log(`Kanst du mir sagen was ist: ` + feiertagen[1].name);
+        
+
+        for (let i=0; i< feiertagen.length; i++){
+            if( globalDate.getTime() == feiertagen[i].date.getTime()){
+                
+                document.getElementById(`infoHoliday`).innerHTML = `Es handelt sich um ${feiertagen[i].name} einen gesetzlichen Feiertag`;
+                tagCell.classList.add(`feiertag`);
+                
+            }
+            else{
+                document.getElementById(`infoHoliday`).innerHTML = `Es handelt sich nicht um einen gesetzlichen Feiertag`;
+            } 
+        }
+    }   
+    }   
     
     //Funktion, die "tag" Zelle erstellt
 
-    function tagCellCreator (day, otherMonth, today) {
+    function tagCellCreator (day, otherMonth, today, feiertag) {
 
         let day2 = new Date(day);
 
@@ -258,21 +316,22 @@ function calenderSheet () {
         
         }
 
-        // if (feiertag){
-        //     tagCell.classList.add("feiertag"); 
-        // }
-
-        if (otherMonth) {
-
-            tagCell.classList.add("otherMonth");
         
+        if (otherMonth) {
+            
+            tagCell.classList.add("otherMonth");
+            
         }
-       
+        
+        
+        if (feiertag) {
+            tagCell.classList.add("feiertag"); 
+        }
 
         if (day2.getDay() == 0 || day2.getDay() == 6) {
-
+            
             tagCell.classList.add("weekend");
-
+            
         }    
 
  
@@ -306,49 +365,6 @@ function calenderSheet () {
     });
     
 }
-
-
-// Ostern Formel 
-// funktion holidays(){ 
-
-//     let date= new Date ();
-//     let dateD = date.getDate();
-//     let year = date.getFullYear();
-
-//     In getMonth ist der Wert für Moaten ab 0 anfängt. Es heißt, dass Januar=0 und Dezember=11. Um Monate bequemer in Algorithmus eingeben zu können, würde getMonth + 1 geschrieben
-//     let monthD = date.getMonth () + 1;
-//     let holidayYesOrNo;
-// const a = year % 4;
-// const b = year % 7;
-// const c = year % 19;
-// const d = (19*c +24) % 30;
-// const e = (2*a + 4*b + 6*d +5) % 7;
-// const f = (c + 11*d + 22*e) % 451;
-
-// const osternSonntag = Math.floor(22+d+e-7*f);
-// if (osternSonntag > 31){
-//         osternSonntag = Math.floor((22+d+e-7*f) -31); 
- 
-// }
-
-// return osternSonntag;
-// tagCell.classList.add(`osternSonntag`, `feiertag`);
-
-// if ( dateD == 1 && monthD == 1 || osternSonntag - 2 == dateD || osternSonntag == dateD|| osternSonntag + 1 == dateD || dateD == 1 && monthD == 5 || osternSonntag + 39 == dateD || 
-// osternSonntag + 49 == dateD || osternSonntag + 50 == dateD || osternSonntag + 60 == dateD || date == 3 && monthD == 10 || date == 25 && monthD == 12 || date == 26 && monthD == 12) {
-//     tagCell.classList.add(`feiertag`);
-//     holidayYesOrNo ="";
-// }
-// else {
-//     holidayYesOrNo = "nicht"; 
-// }
-
-// document.getElementById(`infoHoliday`).innerHTML = holidayYesNoD;
-
-//  }
-
-
-
 
 
 
